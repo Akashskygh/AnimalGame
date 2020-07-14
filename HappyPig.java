09e2b9cb4a8fd33e3766918ebe3747d186d439a0
Zein Elrez <67756015+Zeinelrez@users.noreply.github.com>
2020-07-14 12:04:33 -0700

    Update HappyPig.java

--- HappyPig.java
+++ HappyPig.java
@@ -60,7 +60,7 @@ public class HappyPig extends Animal
    
     else 
     { 
-      return "No such action is possible, your animal is dead. You shouldn't have abused it."; 
+      return dead_mess; 
     }
   }
  
