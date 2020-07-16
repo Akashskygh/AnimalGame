dbe33b409fd0c592cea7de9319b3eb2b7927cdc6
James Nguyen <minhkhoix01@gmail.com>
2020-07-16 11:56:34 -0700

    Update layout for TestDriver
    I create the layout for TestDriver.

--- TestDriver.java
+++ TestDriver.java
@@ -1,18 +1,40 @@
-import java.awt.Point;
-
+//******************************************************
+// TestDriver.java - This class is for testing 
+// Contributors: Zein, Kelsee, Akash, James 
+//******************************************************
+import java.awt.Point; //using Point 2D
 public class TestDriver
 {
      public static void main (String[] args)
      {
+         
+          // Creating (4) characters 
           CuriousBunny bunny = new CuriousBunny ("White", "Bugs");
           HappyPig pig = new HappyPig ("Pink", "Peppa");
           AngryDuck duck = new AngryDuck ("Black", "Donald");
           LoudGoose goose = new LoudGoose ("Yellow", "Daisy");
           
+          //Testing Actions. 
           bunny.hop();
+          
+          //----------------------------------------------------------------
+          //Testing Teleport. (Only CuriousBunny && AngryDuck can teleport)
+          //----------------------------------------------------------------
+          //display default positions.  
           System.out.print("\n"+bunny.getPosition());
-     
+          System.out.print("\n"+duck.getPosition());
+          
+          //using teleport(). 
+          bunny.teleport(100,12.5); 
+          duck.teleport(-1,2); 
+          
+          //display new positions. 
+          System.out.print( "\n"+bunny.getPosition() );
+          System.out.print( "\n"+duck.getPosition() );
+          //--------------------------------------------------------------
+          //Testing Attackers. (Only AngryDuck && LoudGoose are attackers)
+          //--------------------------------------------------------------
+          
      }// end of main()
-     
-}// end of class
+}// end of class.
      
