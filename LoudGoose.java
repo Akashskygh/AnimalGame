6bd7db9fb811d02165bbbe10e24a500b58c5dfbd
Zein Elrez <67756015+Zeinelrez@users.noreply.github.com>
2020-07-16 09:53:11 -0700

    Update LoudGoose.java

--- LoudGoose.java
+++ LoudGoose.java
@@ -6,35 +6,5 @@ public class LoudGoose extends WaterFowl
   { 
     super(color, name); 
   }
-  
-  public String fly()
-  { 
-    return super.fly();
-  }
-  
-  public String land()
-  { 
-    return super.land();
-  }
-  
-  public String swim()
-  { 
-    return super.swim();
-  }  
- 
-  public boolean fly_state()
-  { 
-    return super.fly_state();
-  }
-  
-   public void attack(Animal name1) 
-  {
-    super.attack(name1); 
-  } 
-  
-  public int getKillCount()
-  {
-    return super.getKillCount();
-  }
  
 }
