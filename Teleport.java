56143a1e57957a67e22379d08943f8d0c5836b93
James Nguyen <minhkhoix01@gmail.com>
2020-07-16 03:23:58 -0700

    Update Teleport.java
    Added new abstract methods

--- Teleport.java
+++ Teleport.java
@@ -10,6 +10,14 @@ public interface Teleport
      //method getPosition() - get character and return its position as a String
      //-------------------------------------------------------------------------
      public String getPosition(); 
+     //-----------------------------------------------
+     //method getX() - get X coordinate of character
+     //-----------------------------------------------
+     public double getX();
+     //-----------------------------------------------
+     //method getY() - get Y coordinate of character
+     //-----------------------------------------------
+     public double getY();
      //------------------------------------------------------------------------------------
      //method teleport() - get position from user and teleport character to given position
      //------------------------------------------------------------------------------------
