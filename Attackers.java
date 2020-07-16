03ec592aad2161a94c1c6086830e52c3471b646a
James Nguyen <minhkhoix01@gmail.com>
2020-07-16 11:38:36 -0700

    Update Attackers.java

--- Attackers.java
+++ Attackers.java
@@ -1,6 +1,15 @@
-
+//*******************************************
+// Attackers.java is Interface
+// contributors: Zein, Kelsee, Akash, James
+//*******************************************
 public interface Attackers
 {
-  public void attack(Animal name1); 
+  //---------------------------------------
+  //attack() - to attack others characters
+  //---------------------------------------
+  public void attack(Animal name); 
+  //-------------------------------------------------
+  //getKillCount() - to count kill the character got
+  //-------------------------------------------------
   public int getKillCount();
-} 
+}//end of Attackers. 
