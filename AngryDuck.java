df1d3a040da02bdb67844e03aaf78918273d88bb
James Nguyen <minhkhoix01@gmail.com>
2020-07-16 10:18:05 -0700

    Update AngryDuck.java

--- AngryDuck.java
+++ AngryDuck.java
@@ -2,6 +2,7 @@ import java.awt.Point; //import Point 2D
 public class AngryDuck extends WaterFowl implements Teleport
 { 
      //Declare varibles.
+     Point location = new Point (0,0); //default is (0,0) when created.
      //-----------------------------------------
      // AngryDuck's only constructor
      //-----------------------------------------
@@ -12,8 +13,55 @@ public class AngryDuck extends WaterFowl implements Teleport
           
      }//end of constructor.
      
+     public boolean fly_state()
+     {
+          return super.fly_state();  
+     } 
+     
+     public void attack(Animal name1) 
+     {
+          super.attack(name1); 
+     } 
+     
+     public int getKillCount()
+     {
+          return super.getKillCount();
+     }
+     
      //-------------------------------------------------------------------------
      //method getPosition() - get character and return its position as a String
      //-------------------------------------------------------------------------
-     
+     public String getPosition()
+     {
+          return "("+ location.getX() + ", "+ location.getY() + ")";
+          
+     }//end of getPostion(). 
+     //-----------------------------------------------
+     //method getX() - get X coordinate of character
+     //-----------------------------------------------
+     public double getX()
+     {
+          return location.getY()
+     }//end of getX()
+     //-----------------------------------------------
+     //method getY() - get Y coordinate of character
+     //-----------------------------------------------
+     public double getY()
+     {
+          return location.getX();
+     }//end of getY()
+     //---------------------------------------------
+     // checkCoord() - check validity of location
+     //---------------------------------------------
+     public void checkCoord()
+     {
+          //Kelsee please help me with this. 
+     }//end of checkCoord().
+     //------------------------------------------------------------------------------------
+     //method teleport() - get position from user and teleport character to given position
+     //------------------------------------------------------------------------------------
+     public void teleport(double x, double y)
+     { 
+       //to be completed - Kels
+     }//end of teleport(). 
 }//end of AngryDuck class. 
