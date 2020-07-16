8a9da52e95866ddbfb952ca64a8c8f862ef0b7c2
Zein Elrez <67756015+Zeinelrez@users.noreply.github.com>
2020-07-16 09:56:43 -0700

    Update AngryDuck.java

--- AngryDuck.java
+++ AngryDuck.java
@@ -2,7 +2,6 @@ import java.awt.Point; //import Point 2D
 public class AngryDuck extends WaterFowl implements Teleport
 { 
      //Declare varibles.
-     Point location = new Point (0,0); //default is (0,0) when created.
      //-----------------------------------------
      // AngryDuck's only constructor
      //-----------------------------------------
@@ -16,37 +15,5 @@ public class AngryDuck extends WaterFowl implements Teleport
      //-------------------------------------------------------------------------
      //method getPosition() - get character and return its position as a String
      //-------------------------------------------------------------------------
-     public String getPosition()
-     {
-          return "("+ location.getX() + ", "+ location.getY() + ")";
-          
-     }//end of getPostion(). 
-     //-----------------------------------------------
-     //method getX() - get X coordinate of character
-     //-----------------------------------------------
-     public double getX()
-     {
-          return location.getY()
-     }//end of getX()
-     //-----------------------------------------------
-     //method getY() - get Y coordinate of character
-     //-----------------------------------------------
-     public double getY()
-     {
-          return location.getX();
-     }//end of getY()
-     //---------------------------------------------
-     // checkCoord() - check validity of location
-     //---------------------------------------------
-     public void checkCoord()
-     {
-          //Kelsee please help me with this. 
-     }//end of checkCoord().
-     //------------------------------------------------------------------------------------
-     //method teleport() - get position from user and teleport character to given position
-     //------------------------------------------------------------------------------------
-     public void teleport(double x, double y)
-     { 
-       //to be completed - Kels
-     }//end of teleport(). 
+     
 }//end of AngryDuck class. 
