f88c0e4ec7e518d631f297fc705287dedc0a7d9a
Zein Elrez <67756015+Zeinelrez@users.noreply.github.com>
2020-07-16 09:57:39 -0700

    Update CuriousBunny.java

--- CuriousBunny.java
+++ CuriousBunny.java
@@ -3,7 +3,7 @@ import java.awt.Point;
 public class CuriousBunny extends Animal implements Teleport
 {
      // Declare varibles. 
-     protected Point location = new Point(0,0); //default location is (0,0) when character is created
+
      //---------------------------------------------------------
      //CuriousBunny's constructor.(this is the only constructor)  
      //---------------------------------------------------------
@@ -23,37 +23,6 @@ public class CuriousBunny extends Animal implements Teleport
      //-------------------------------------------
      //getPosition - return position of character
      //-------------------------------------------
-     public String getPosition()
-     {
-       return "("+ location.getX() + ", "+ location.getY() + ")";
-     }
-     //-----------------------------------------------
-     //method getX() - get X coordinate of character
-     //-----------------------------------------------
-     public double getX()
-     {
-          return location.getY()
-     }//end of getX()
-     //-----------------------------------------------
-     //method getY() - get Y coordinate of character
-     //-----------------------------------------------
-     public double getY()
-     {
-          return location.getX();
-     }//end of getY()
-     //---------------------------------------------
-     // checkCoord() - check validity of location
-     //---------------------------------------------
-     public void checkCoord()
-     {
-          //Kelsee please help me with this. 
-     }//end of checkCoord().
-     //----------------------------------------------------
-     //teleport()- teleport character to given coordinates 
-     //----------------------------------------------------
-     public void teleport(double x, double y)
-     {
-       //To be completed - Kels
-     }
+     
      
 }//end of CuriousBunny class.
