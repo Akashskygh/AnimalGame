431712e10a054e66f02b4041328e5a670ffe992d
James Nguyen <minhkhoix01@gmail.com>
2020-07-15 23:04:27 -0700

    Update CuriousBunny.java
    + location should be the varible of this class. not only the constructor. 
    + removed parameters of getPosition() .
    +change type of teleport parameters from int -> double.

--- CuriousBunny.java
+++ CuriousBunny.java
@@ -2,14 +2,15 @@ import java.awt.Point;
 
 public class CuriousBunny extends Animal implements Teleport
 {
+     // Declare varibles. 
+     Point location = new Point(0,0); //default location is (0,0) when character is created
      //---------------------------------------------------------
      //CuriousBunny's constructor.(this is the only constructor)  
      //---------------------------------------------------------
      public CuriousBunny(String color, String name)
      { 
           super(color, name);
-          String teleporter = "Bunny";      //this is Bunny. 
-          Point location = new Point(0,0); //default location is (0,0) when character is created
+          location = new Point(0,0); //Bunny can teleport so it will have location. 
      }//end of CuriousBunny
      
      //-------------------------------
@@ -20,12 +21,12 @@ public class CuriousBunny extends Animal implements Teleport
           doAction("is hopping");
      }//end of hop(). 
      
-     public String getPosition(Animal name1)
+     public String getPosition()
      {
        return "("+ location.getX() + ", "+ location.getY() + ")";
      }
      
-     public void teleport(int x, int y)
+     public void teleport(double x, double y)
      {
        //To be completed - Kels
      }
