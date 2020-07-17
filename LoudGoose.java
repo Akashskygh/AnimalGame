17be836ab0dd0f7838d40499e87e5272f5ca519a
James Nguyen <minhkhoix01@gmail.com>
2020-07-16 18:36:06 -0700

    Update LoudGoose.java
    comments added

--- LoudGoose.java
+++ LoudGoose.java
@@ -1,9 +1,14 @@
-
+//*****************************************
+// LoudGoose.java 
+// Contributors: Zein, Kelsee, Akash, James
+//*****************************************
 public class LoudGoose extends WaterFowl
 {
+  //----------------------------------------
+  // LoudGoose's only constructor
+  //----------------------------------------
   public LoudGoose(String color, String name)
   { 
     super(color, name); 
-  }
- 
-}
+  }//end of LoudGoose's constructor.
+}//end of class.
