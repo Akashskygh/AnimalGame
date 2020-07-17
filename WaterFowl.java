45a7cc50df677044ade83f68414641fb6aa9477c
James Nguyen <minhkhoix01@gmail.com>
2020-07-16 19:48:57 -0700

    Update WaterFowl.java

--- WaterFowl.java
+++ WaterFowl.java
@@ -1,117 +1,99 @@
 
+//*****************************************************************
+// WaterFowl.java - This is abstract class for Duck && Goose
+// Contributors: Zein, Kelsee, Akash, James. 
+//*****************************************************************
 public abstract class WaterFowl extends Animal implements Attackers
 { 
-  protected boolean fly_state = false; 
-  protected int kills = 0; 
-  
-  public WaterFowl(String color, String name) 
-  { 
-     super(color,name); 
-  } 
-    
-  public String fly()
-  {
-     if ( health > 0 )
+     //declare varibles.
+     protected boolean fly_state = false; // state (flying/landing) of character.
+     protected int kills = 0;             // kills that character got.
+     
+     //-----------------------------------------
+     // WaterFowl's only construct
+     //-----------------------------------------
+     final public WaterFowl(String color, String name) 
      { 
-       if ( fly_state == true) 
-       {  
-         return "Duck is still flying and had not yet landed.";
-       }
-       else
-       { 
-         fly_state = true; 
-         return name + " is flying...";
-       }
-     }
-
-     else  
+          super(color,name); 
+     } //end of constructor. 
+     
+     //----------------------------------------
+     // fly() - do action flying
+     //----------------------------------------
+     final public String fly()
+     {
+          if ( health > 0 ) { 
+               if ( fly_state == true) {  
+                    return "Duck is still flying and had not yet landed.";
+               } else { 
+                    fly_state = true; 
+                    return name + " is flying...";
+               }
+          } else  {  
+               return "No such action is possible, your animal is dead. You shouldn't have abused it."; 
+          }
+     }//end of fly(). 
+     
+     //------------------------------------
+     // land() - do action landing
+     //------------------------------------
+     final public String land()
      {  
-       return "No such action is possible, your animal is dead. You shouldn't have abused it."; 
-     }
-  }
-  
-  public String land()
-  {  
-     if ( health > 0 )
+          if ( health > 0 ) { 
+               if (fly_state == false) { 
+                    return "Duck isn't flying..."; 
+               } else {
+                    fly_state = false; 
+                    return name + " is landing...";
+               }
+          } else { 
+               return "No such action is possible, your animal is dead. You shouldn't have abused it." ; 
+          }
+     }//end of land(). 
+     
+     //--------------------------------------
+     // swim() - do action swimming
+     //--------------------------------------
+     final public String swim()
      { 
-       if (fly_state == false) 
-       { 
-         return "Duck isn't flying..."; 
-       }
-      
-       else 
-       {
-         fly_state = false; 
-         return name + " is landing...";
-       }
-     }
-   
-     else 
+          doAction(" is swimming...");
+     }//end of swim().
+     
+     //---------------------------------------
+     // fly_state() - return flying state
+     //----------------------------------------
+     final public boolean fly_state()
      { 
-       return "No such action is possible, your animal is dead. You shouldn't have abused it." ; 
-     }
-  }
-  
-  public String swim()
-  { 
-     if ( health > 0 )
-     { 
-       return name + " is swimming...";
-     }
-   
-     else 
-     { 
-       return "No such action is possible, your animal is dead. You shouldn't have abused it."; 
-     }
-    
-  }
-   
-  public boolean fly_state()
-  { 
-     return fly_state;
-  }
-  
-  public void attack(Animal name1) 
-  { 
-    
-    if ( health == 0)
-    { 
-      System.out.println("Your animal cannot attack because it's dead"); 
-    } 
-  
-    else if ( name1.getClass().getSimpleName().compareTo(this.getClass().getSimpleName()) == 0) 
-    { 
-      System.out.println("Your animal cannot attack from its own type");
-    }  
-    
-    else if (name1.health == 0 ) 
-    { 
-      System.out.println("You cannot hit a dead animal"); 
-    } 
-    
-    else if (name1.health == 1) 
-    { 
-      kills++; 
-      name1.hit(1); 
-      System.out.println("Succesful strike, the animal is now Dead"); 
-    } 
-    
-    else
-    { 
-      System.out.println("Succesful strike, the animal is now Hit"); 
-      name1.hit(1); 
-    } 
-   
-  }
-  
-  public int getKillCount()
-  {
-    return kills;
-  }
-       
-    
-  public String toString() 
-  {  
-     return super.toString();   
-  }   
-}
+          return fly_state;
+     }//end of fly_state(). 
+     
+     //--------------------------------------
+     // attack() - attack other players. 
+     //---------------------------------------
+     final public void attack(Animal name1) 
+     {   
+          if ( health == 0)
+          { 
+               System.out.println("Your animal cannot attack because it's dead"); 
+          } else if ( name1.getClass().getSimpleName().compareTo(this.getClass().getSimpleName()) == 0) {
+               System.out.println("Your animal cannot attack from its own type");
+          }  else if (name1.health == 0 ) {
+               System.out.println("You cannot hit a dead animal"); 
+          } else if (name1.health == 1) { 
+               kills++; 
+               name1.hit(1); 
+               System.out.println("Succesful strike, the animal is now Dead"); 
+          } else { 
+               System.out.println("Succesful strike, the animal is now Hit"); 
+               name1.hit(1); 
+          }    
+     }//end of attack(). 
+     
+     //-------------------------------------------------
+     // getKillCount() - return kills that character got
+     //-------------------------------------------------
+     final public int getKillCount()
+     {
+          return kills;
+     }//end of getKillCount().
+}//end of class. 
